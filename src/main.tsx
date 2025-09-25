220596c65753e7eb4b3cbf136f38b5c2f6300f39
Mimi Reyburn <mimireyburn@gmail.com>
2025-09-25 19:00:59 +0100

    try getting rm get-snap

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
-import GetSnap from './get-snap/get-snap.tsx'
+// import GetSnap from './get-snap/get-snap.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<App />} />
-				<Route path="/get-snap" element={<GetSnap />} />
+				{/* <Route path="/get-snap" element={<GetSnap />} /> */}
 			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>
